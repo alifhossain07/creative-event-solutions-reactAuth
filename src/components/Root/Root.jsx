4a4cdba140df334feef28723752adacdab0beb9a
Alif Hossain <alifhossain56782@gmail.com>
2024-09-09 03:02:07 +0600

    Footer Done

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div>
         <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
