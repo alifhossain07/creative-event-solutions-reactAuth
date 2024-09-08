3c54813c38728a81245e3535401dbb273ce97702
Alif Hossain <alifhossain56782@gmail.com>
2024-09-08 21:55:57 +0600

    Doing The Slider

--- src/Slides/Slides.jsx
+++ src/Slides/Slides.jsx
@@ -3,36 +3,50 @@ import React, { useState } from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./style.css"
+import Slide from "../Slide/Slide";
+
 
 
 const Slides = ({sliderData}) => {
 
+
+    const {short_details,title} = sliderData;
+
     const [currentSlide, setCurrentSlide] = useState(0)
-    const [loaded, setLoaded] = useState(false)
+    const [loaded, setLoaded] = useState(false);
+    const [fadeClass, setFadeClass] = useState("fade-enter-active"); 
+
+
     const [sliderRef, instanceRef] = useKeenSlider({
     initial: 0,
     slideChanged(slider) {
       setCurrentSlide(slider.track.details.rel)
+      
     },
     created() {
       setLoaded(true)
     },
+   
   })
-
-
+  
+  
 
 
     return (
         <div className="mt-10">
             <div className="navigation-wrapper">
-        <div ref={sliderRef} className="keen-slider">
-          <div className="keen-slider__slide number-slide1">1</div>
-          <div className="keen-slider__slide number-slide2">2</div>
-          <div className="keen-slider__slide number-slide3">3</div>
-          <div className="keen-slider__slide number-slide4">4</div>
-          <div className="keen-slider__slide number-slide5">5</div>
-          <div className="keen-slider__slide number-slide6">6</div>
+
+
+        <div ref={sliderRef} className="keen-slider rounded-2xl">
+
+        {sliderData.map((slide) => (
+            <Slide slide={slide} ></Slide>
+          ))}
         </div>
+
+
+
+
         {loaded && instanceRef.current && (
           <>
             <Arrow
